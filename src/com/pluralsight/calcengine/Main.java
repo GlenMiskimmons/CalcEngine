9f6feb93d5ac6e3b22e2d4d2ef9269ee8d82f00f
GlenMiskimmons <glenmiskimmons@gmail.com>
2018-09-25 22:04:36 +0100

    Pluralsight Course
    Java Fundamentals: The Java Language

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -20,17 +20,15 @@ public class Main {
 
         for(MathEquation equation : equations) {
             equation.execute();
-            System.out.println("result = " + equation.result);
+            System.out.println("result = " + equation.getResult());
         }
-
-
     }
 
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
         MathEquation equation = new MathEquation();
-        MathEquation.leftVal = leftVal;
-        MathEquation.rightVal = rightVal;
-        MathEquation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
 
         return equation;
     }
